fed40c3275ca1ea6e68cacb62dd01482a3ba6cf4
luckel92 <129117205+luckel92@users.noreply.github.com>
2023-04-15 13:12:30 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var garden,rabbit,apple,red,orange;
+var gardenImg,rabbitImg,appleImg,redImg,redImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+ orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png"); 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// mover o fundo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//criar sprite do coelho
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+function createApples(){
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+}
+createApples();
+function draw() {
+  background(0);
+
+
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+  
+}
